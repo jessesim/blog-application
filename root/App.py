e93d626ab5e8ec24487b4e1b441b248fc91001cd
Jesse <jessesimmons12@yahoo.com>
2024-08-14 13:32:24 -0400

    Started repoistory

---
+++ root/App.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        new_post = Post(title=request.form['title'], content=request.form['content'])
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/update/<int:post_id>', methods=['GET','POST'])
+def update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', post=post)
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
